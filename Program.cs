bd1186a3a7bce8b052fbf17a760ec1beb86b55ad
Milton Pérez <milton.perez20017798@estu.unan.edu.ni>
2022-05-31 08:48:31 -0600

    Agregando comentario sobre que hace el programa

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿//programa que permita visualizar los departamentos de nicaragua con su cantidad poblacional.
+//Encuentre: Mayor, menor, sumas y ordene los datos
+using System.Collections;
+Dictionary<string, int> dep = new Dictionary<string, int>()
+{
+    {"Boaco",185013},
+    {"Carazo",197139},
+    {"Chinandega",439906},
+    {"Chontales",190863},
+    {"Costa Caribe Norte",530586},
+    {"Costa Caribe Sur",414543},
+    {"Estelí",229866},
+    {"Granada",214317},
+    {"Jinotega",475630},
+    {"León",421050},
+    {"Madriz",174744},
+    {"Managua",1546939},
+    {"Masaya",391903},
+    {"Matagalpa",593503},
+    {"Nueva Segovia",271581},
+    {"Río San Juan",135446},
+    {"Rivas",182645}
+};
+
+int maxDep = dep.Values.ToArray().Max();
+int minDep = dep.Values.ToArray().Min();
+string maxDepkey = "", minDepkey = "";
+
+foreach (var item in dep.Keys)
+{
+    if (dep[item] == maxDep)
+        maxDepkey = item;
+    if (dep[item] == minDep)
+        minDepkey = item;
+    if (minDepkey != "" && maxDepkey != "")
+        break;
+}
+
+var orderDepResult=dep.OrderBy(d=>d.Value);
+
+
+foreach (var item in orderDepResult)
+    Console.WriteLine($"{item.Key,20} ==> {item.Value,10:N0}");
+
+Console.WriteLine($"Población General:{dep.Values.Sum():N0}");
+Console.WriteLine($"Departamento con mayor Población:{maxDepkey}");
+Console.WriteLine($"Departamento con menor Población:{minDepkey}");
